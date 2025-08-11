81048264563fcf149b1d10d3b0679a07e9525208
arroo <Pm9521263317@gmail.com>
2025-08-11 13:56:05 +0530

    fix

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { requireAuth } from "../middlewares/auth.middleware.js";
-import {cancelSubscription, createSubscription, getMySubscription, updateSubscription} from "../controllers/subscription.controller.js"
+import {cancelSubscription, createOrRenewSubscription, getMySubscription, updateSubscription} from "../controllers/subscription.controller.js"
 
 const router = express.Router();
 router.use(requireAuth)
 
-router.post("/create-subcription", createSubscription);
+router.post("/create-subcription", createOrRenewSubscription);
 router.get("/get-my-subcription", getMySubscription);
 router.put("/renew-subcription", updateSubscription);
 router.delete("/cancel-subcription", cancelSubscription);
