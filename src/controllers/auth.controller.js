6057ad8d33b6550a8499e0da1980f2c3e5768e78
arroo <Pm9521263317@gmail.com>
2025-08-14 15:20:34 +0530

    `Refactor cookie options for access and refresh tokens`

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,11 +11,19 @@ import {
 } from "../utils/lib.js";
 
 
-const cookieOptions = {
-  httpOnly: true, // Browser JS se access na ho
-  secure: false, // Local dev me HTTPS nahi hota, isliye false
-  sameSite: "None", // Cross-site requests allow kare
+const accessTokenOptions = {
+   httpOnly: true,
+  secure: false,
+  sameSite: "None",
   path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+};
+const refreshTokenOptions = {
+   httpOnly: true,
+  secure: false,
+  sameSite: "None",
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 
@@ -109,8 +117,8 @@ const login = asyncHandler(async (req, res) => {
     const { password: _, ...userSafe } = user;
     return res
       .status(200)
-      .cookie("accessToken", accessToken, cookieOptions)
-      .cookie("refreshToken", refreshToken, cookieOptions)
+      .cookie("accessToken", accessToken, accessTokenOptions)
+      .cookie("refreshToken", refreshToken, refreshTokenOptions)
       .json(new ApiResponse(200, "Login successful", {
         user: userSafe,
         accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRY || "7d",
