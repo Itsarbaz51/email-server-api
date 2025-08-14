f382828c3912260399482059c595f47a096abb4c
arroo <Pm9521263317@gmail.com>
2025-08-14 11:01:46 +0530

    `Fixed jwt expiresIn syntax in generateAccessToken and generateRefreshToken functions`

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -15,11 +15,11 @@ export const comparePassword = async (password, hashedPassword) => {
 export const generateAccessToken = (id, email, role) => {
   const secret = process.env.ACCESS_TOKEN_SECRET;
   const expiresIn = process.env.ACCESS_TOKEN_EXPIRY || "7d";
-  return jwt.sign({ id, email, role }, secret, { expiresIn });
+  return jwt.sign({ id, email, role }, secret, { expiresIn:expiresIn });
 };
 
 export const generateRefreshToken = (id, email, role) => {
   const secret = process.env.REFRESH_TOKEN_SECRET;
   const expiresIn = process.env.REFRESH_TOKEN_EXPIRY || "90d";
-  return jwt.sign({ id, email, role }, secret, { expiresIn });
+  return jwt.sign({ id, email, role }, secret, { expiresIn:expiresIn });
 };
