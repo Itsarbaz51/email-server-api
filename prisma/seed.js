028ddfbec777372e57b29618e7ec7ad9dd97869c
arroo <Pm9521263317@gmail.com>
2025-08-21 12:55:34 +0530

    add seef

---
+++ prisma/seed.js
@@ -0,0 +1,50 @@
+import Prisma from "../src/db/db.js";
+import { comparePassword } from "../src/utils/lib.js";
+
+async function main() {
+  const superAdminEmail = "azzunique.com@gmail.com";
+  const superAdminPhone = " 7412066471";
+  const superAdminPassword = "Azz@181883";
+
+  const exists = await Prisma.user.findFirst({
+    where: {
+      OR: [
+        { email: superAdminEmail },
+        { phone: superAdminPhone },
+        { role: "SUPER_ADMIN" },
+      ],
+    },
+  });
+
+  if (exists) {
+    console.log("✅ Superadmin already exists:", exists.email);
+    return;
+  }
+
+  const hashed = await comparePassword(superAdminPassword);
+
+  const superAdmin = await Prisma.user.create({
+    data: {
+      name: "Super Admin",
+      email: superAdminEmail,
+      phone: superAdminPhone,
+      password: hashed,
+      role: "SUPER_ADMIN",
+      termsAndConditions: true,
+      isActive: true,
+      isAuthorized: true,
+    },
+  });
+
+  console.log("🎉 Superadmin created successfully:", superAdmin.email);
+}
+
+main()
+  .then(async () => {
+    await Prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await Prisma.$disconnect();
+    process.exit(1);
+  });
