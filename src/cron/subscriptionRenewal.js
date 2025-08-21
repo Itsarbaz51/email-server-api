ec4c11d08100cb4b9004921c8fcbfc645cdadb1b
arroo <Pm9521263317@gmail.com>
2025-08-21 11:47:27 +0530

    `Update cron schedule in subscriptionRenewal.js to run every minute`

--- src/cron/subscriptionRenewal.js
+++ src/cron/subscriptionRenewal.js
@@ -2,7 +2,8 @@ import cron from "node-cron";
 import Prisma from "../db/db.js";
 import { generateInvoiceId } from "../utils/lib.js";
 
-cron.schedule("*/1 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
+  console.log("⏰ Cron chal gaya!");
   console.log("🔄 Running daily subscription renewal check...");
 
   const today = new Date();
