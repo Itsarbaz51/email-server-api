53b069f11b0eba9a3772a8c20963b77639226c19
arroo <Pm9521263317@gmail.com>
2025-08-25 12:15:41 +0530

    `Added import of ApiResponse from utils/ApiResponse.js`

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,5 +1,6 @@
 import Prisma from "../db/db.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const createTestimonial = asyncHandler(async (req, res) => {
